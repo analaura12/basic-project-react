68d46c479bc97a956a42611f444c66fe54cab752
Ana Laura <ana.laura@unioit.com>
2022-06-14 20:58:53 -0300

    Lifting State Up

--- dist.js
+++ dist.js
@@ -1,11 +1,13 @@
 const NomeContext = React.createContext('nome');
 
-function MeuComponente1() {
+function MeuComponente1(props) {
   //I want to pass this variable to the component called MeuComponente4
   const meuNome = 'Ana Laura Almeida';
   return /*#__PURE__*/React.createElement("div", {
     className: "component-1"
-  }, /*#__PURE__*/React.createElement(MeuComponente2, null, /*#__PURE__*/React.createElement("p", null, "Hello! How are you?")));
+  }, /*#__PURE__*/React.createElement(MeuComponente2, null, /*#__PURE__*/React.createElement(MeuComponente3, {
+    onClickIncrementar: props.onClickIncrementar
+  })));
 }
 
 function MeuComponente2(props) {
@@ -17,7 +19,9 @@ function MeuComponente2(props) {
 function MeuComponente3(props) {
   return /*#__PURE__*/React.createElement("div", {
     className: "component-3"
-  }, /*#__PURE__*/React.createElement(MeuComponente4, null));
+  }, /*#__PURE__*/React.createElement(MeuComponente4, {
+    onClickIncrementar: props.onClickIncrementar
+  }));
 }
 
 function MeuComponente4(props) {
@@ -27,13 +31,44 @@ function MeuComponente4(props) {
   }, 1000);
   return /*#__PURE__*/React.createElement("div", {
     className: "component-4"
-  }, /*#__PURE__*/React.createElement("p", null, "Component - 4 - ", idade));
+  }, /*#__PURE__*/React.createElement("p", null, "Component - 4 - ", idade), /*#__PURE__*/React.createElement("button", {
+    type: "button",
+    onClick: props.onClickIncrementar
+  }, "Incrementar"));
 }
 
-function MeuComponente() {
+function MeuComponente(props) {
   return /*#__PURE__*/React.createElement("div", {
     id: "components"
-  }, /*#__PURE__*/React.createElement(MeuComponente1, null));
+  }, /*#__PURE__*/React.createElement(MeuComponente1, {
+    onClickIncrementar: props.onClickIncrementar
+  }));
 }
 
-ReactDOM.render( /*#__PURE__*/React.createElement(MeuComponente, null), document.getElementById("app"));
+function MeuComponenteIrmao(props) {
+  return /*#__PURE__*/React.createElement("div", {
+    id: "component-irmao"
+  }, /*#__PURE__*/React.createElement(MeuComponenteIrmao2, {
+    contador: props.contador
+  }));
+}
+
+function MeuComponenteIrmao2(props) {
+  return /*#__PURE__*/React.createElement("h2", null, "Contador: ", props.contador, " ");
+}
+
+function AppComponent() {
+  const [contador, setContador] = React.useState(0);
+
+  const clickIncrementa = function () {
+    setContador(contador + 1);
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(MeuComponente, {
+    onClickIncrementar: clickIncrementa
+  }), /*#__PURE__*/React.createElement(MeuComponenteIrmao, {
+    contador: contador
+  }));
+}
+
+ReactDOM.render( /*#__PURE__*/React.createElement(AppComponent, null), document.getElementById("app"));
--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const NomeContext = React.createContext('nome');
 
-function MeuComponente1() {
+function MeuComponente1(props) {
     //I want to pass this variable to the component called MeuComponente4
     const meuNome = 'Ana Laura Almeida';
     return (
         <div className="component-1">
             <MeuComponente2>
-                <p>Hello! How are you?</p>
+                <MeuComponente3 onClickIncrementar={props.onClickIncrementar}></MeuComponente3>
             </MeuComponente2>
         </div>
     )
@@ -27,7 +27,7 @@ function MeuComponente2(props) {
 function MeuComponente3(props) {
     return (
         <div className="component-3">
-            <MeuComponente4></MeuComponente4>
+            <MeuComponente4 onClickIncrementar={props.onClickIncrementar}></MeuComponente4>
         </div>
     )
 }
@@ -37,22 +37,54 @@ function MeuComponente4(props) {
     setTimeout(function(){
         setIdade(29);
     }, 1000)
+
     return (
         <div className="component-4">
             <p>Component - 4 - {idade}</p>
+            <button type="button" onClick={props.onClickIncrementar}>Incrementar</button>
         </div>
     )
 }
 
-function MeuComponente() {
+function MeuComponente(props) {
     return (
         <div id="components">
-            <MeuComponente1></MeuComponente1>
+            <MeuComponente1 onClickIncrementar={props.onClickIncrementar}></MeuComponente1>
         </div>
+    )
+}
+
+function MeuComponenteIrmao(props){
+    return(
+        <div id="component-irmao">
+            <MeuComponenteIrmao2 contador={props.contador}></MeuComponenteIrmao2>
+        </div>
+    )
+}
+
+
+function MeuComponenteIrmao2(props){
+    return(
+        <h2>Contador: {props.contador} </h2>
+    )
+}
+
+function AppComponent(){
+    const [contador, setContador] = React.useState(0);
+
+    const clickIncrementa = function(){
+        setContador(contador + 1);
+    }
+
+    return(
+        <React.Fragment>     
+            <MeuComponente onClickIncrementar={clickIncrementa}/>
+            <MeuComponenteIrmao contador={contador}/>
+        </React.Fragment>
     )
 }
 
 ReactDOM.render(
-    <MeuComponente></MeuComponente>,
+    <AppComponent></AppComponent>,
     document.getElementById("app")
 )
